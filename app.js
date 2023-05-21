ad5a74fbc7d40e9a929a36ef54915220d07bbdc1
AmberYounus <65040261+AmberYounus@users.noreply.github.com>
2023-05-21 12:55:32 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 //to load google chart
 google.charts.load("current" ,{'packages' : ['corechart']});
 google.charts.setOnLoadCallback(drawChart);
+
+//Draw the chart and put any values
+function drawChart(){
+    var data =google.visualization.arrayToDataTable([
+        ['Task' ,'Hours per Day'],
+        ['Coding',1.5],
+        ['Eat',2],
+        ['Watching Videos',5],
+        ['Sleep',8],
+        ['Relaxation',1],
+     
+    ])
